f73392e0eacc6301f38a0a8c656e404187a2c616
jojo202ajo <ergesbajuuluakzol@gmail.com>
2021-08-04 15:02:23 +0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,18 @@
+package com.company;
+
+import java.util.Scanner;
+
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        Scanner myOdj = new Scanner(System.in );
+        System .out.println(" Enter your name");
+
+        String userName = myOdj.nextLine();
+        System.out.println (" hi " + userName);
+
+        
+    }
+}
